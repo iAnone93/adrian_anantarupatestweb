805a4c6d3842b0b3523a7589e1ac0ce9b4a8696a
iAnone93 <81681657+iAnone93@users.noreply.github.com>
2021-04-26 21:56:51 +0900

    Add files via upload

---
+++ src/Content.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './style.css'
+
+function Content(){
+    return (
+        <div>
+            <section className="skrollable u-clearfix u-image u-parallax u-shading u-section-1" id="sec-885b" data-image-width="1456" data-image-height="819">
+                <div className="u-clearfix u-sheet u-sheet-1">
+                    <h1 className="u-custom-font u-font-oswald u-text u-text-palette-3-base u-text-1">Create a Team, Join the Tournament!</h1>
+                    <h4 className="u-custom-font u-font-oswald u-text u-text-2">Create a team with up to 5 players to join the tournament!</h4>
+                </div>
+            </section>
+            <section className="u-clearfix u-grey-5 u-section-2" id="sec-ade8">
+                <div className="u-clearfix u-sheet u-sheet-1">
+                    <div className="u-clearfix u-expanded-width u-gutter-10 u-layout-wrap u-layout-wrap-1">
+                        <div className="u-layout" >
+                            <div className="u-layout-row" >
+                                <div className="u-container-style u-layout-cell u-left-cell u-size-30 u-size-xs-60 u-layout-cell-1" src="">
+                                    <div className="u-container-layout u-valign-middle u-container-layout-1">
+                                        <h2 className="u-align-center u-custom-font u-font-oswald u-text u-text-1">Anantarupa's Lokapala Tournament</h2>
+                                        <p className="u-align-center u-text u-text-2">Sample text. Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                                        <a href="/" className="u-btn u-btn-round u-button-style u-radius-50 u-btn-1">learn more</a>
+                                    </div>
+                                </div>
+                                <div className="u-align-center u-container-style u-image u-layout-cell u-right-cell u-size-30 u-size-xs-60 u-image-1" src="" data-image-width="800" data-image-height="450">
+                                    <div className="u-container-layout u-valign-middle u-container-layout-2" src=""></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+export default Content
---
+++ src/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './style.css'
+
+function Footer(){
+    return(
+        <div>
+            <footer class="u-align-center u-clearfix u-footer u-grey-80 u-footer" id="sec-99e7">
+                <div class="u-clearfix u-sheet u-sheet-1">
+                    <p class="u-small-text u-text u-text-variant u-text-1">Coming Soon!</p>
+                </div>
+                <div class="u-clearfix u-sheet u-sheet-1">
+                    <p class="u-small-text u-text u-text-variant u-text-1">Copyright, Anantarupa Studios</p>
+                </div>
+            </footer>
+        </div>
+    )
+}
+export default Footer
